746717c1b938e2ca229e8725f2605de108e8cd89
MaksSU <maksy123@gmail.com>
2022-12-23 16:40:22 +0100

    dodanie listy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-const name_max = "Maksymilian Dygasiewicz 'MakSU'"
+const name_list = ["Maksymilian Dygasiewicz 'MakSU'","Makowski Bartosz 'Apocryphen'","Natalia Szwedo 'Baka'"]
 
 function App() {
   return (
     <div className="App">
       <div>
-        {name_max}
+        {name_list.map((item) => <div>{item}</div>)}
       </div>git
       <div>
         Makowski Bartosz 'Apocryphen'

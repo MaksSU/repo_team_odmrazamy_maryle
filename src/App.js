f67a31ba5cf095badf6750b189f39f012952f18f
highwaymanBartus <barutosz007@wp.pl>
2022-12-30 16:31:04 +0100

    zadanko /bartus

--- src/App.js
+++ src/App.js
@@ -7,9 +7,20 @@ const name_list = [
   "Bartosz Godek 'bartus'",
 ];
 
+const updateNameBartus = (name) => {
+  // console.log(name);
+  return name + " 1";
+};
+
 function App() {
   return (
     <div className="App">
+      <div>
+        {name_list.map((item) => (
+          <div>{updateNameBartus(item)}</div>
+        ))}
+      </div>
+
       <div>
         {name_list.map((item) => (
           <div>{item}</div>
